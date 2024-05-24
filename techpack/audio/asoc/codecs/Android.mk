4d16f3ca0b2233f6853c7a3c75cff72055f97250
Alcatraz323 <alcatraz32323@gmail.com>
2024-05-24 19:29:11 +0800

    techpack/audio: add missing build recipe for tfa9878

--- techpack/audio/asoc/codecs/Android.mk
+++ techpack/audio/asoc/codecs/Android.mk
@@ -175,6 +175,14 @@ LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 ###########################################################
+include $(CLEAR_VARS)
+LOCAL_MODULE              := $(AUDIO_CHIPSET)_tfa9878.ko
+LOCAL_MODULE_KBUILD_NAME  := tfa9878_dlkm.ko
+LOCAL_MODULE_TAGS         := optional
+LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+###########################################################
 
 endif # DLKM check
 endif # supported target check
