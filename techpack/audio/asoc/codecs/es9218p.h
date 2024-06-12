44374915338ff35f87bff3f6b42f415dfe12a030
Alcatraz323 <alcatraz32323@gmail.com>
2024-06-12 09:40:30 +0800

    es9218p: call sabre reset when changing es9218_mute() status
    * call a reset when a audio session is first created
    * this will solve the distortion on es9219c

--- techpack/audio/asoc/codecs/es9218p.h
+++ techpack/audio/asoc/codecs/es9218p.h
@@ -240,6 +240,7 @@ struct es9218_priv {
     struct  es9218_data     *es9218_data;
     struct  delayed_work    hifi_in_standby_work;
     struct  delayed_work    sleep_work;
+    struct  delayed_work    plug_headphone_on_work;
     struct  mutex           power_lock;
 } es9218_priv;
 
